f3d5e19ba5df023c77d76d5129272b392d0e8c37
Chris Towles <Chris.Towles@ge.com>
2021-08-17 16:59:53 -0400

    speed and waypoints

--- reward_function.py
+++ reward_function.py
@@ -1,3 +1,14 @@
+# Place import statement outside of function (supported libraries: math, random, numpy, scipy, and shapely)
+# Example imports of available libraries
+#
+# import random
+# import numpy
+# import scipy
+# import shapely
+
+import math
+
+
 def reward_function(params):
     '''
     Example of rewarding the agent to follow center line
@@ -29,40 +40,51 @@ def reward_function(params):
 
     # Read input parameters
     all_wheels_on_track: bool = params['all_wheels_on_track']
+    track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
-
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    
     speed = params['speed']
-    steering = abs(params['steering_angle'])
-    flag_unsafe = params['flag_unsafe']
-    
-    
-    reward = 1e-3  # likely crashed / close to off track
-    
-    # Read input parameters
-    track_width = params['track_width']
-    distance_from_center = params['distance_from_center']
-    
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-    
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
-    
 
-    
+     # Read input variables
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
 
+    # Initialize the reward with typical value
+    reward = 1.0
+
+    # Calculate the direction of the center line based on the closest waypoints
+    # from https://docs.aws.amazon.com/deepracer/latest/developerguide/deepracer-reward-function-input.html#reward-function-input-closest_waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    
+     # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - heading)
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+
+    # Penalize the reward if the difference is too large
+    DIRECTION_THRESHOLD = 10.0
+    if direction_diff > DIRECTION_THRESHOLD:
+        reward *= 0.5
+
+    # Set the speed threshold based your action space
+    SPEED_THRESHOLD = 1.0
+    if not all_wheels_on_track:
+        # Penalize if the car goes off track
+        reward = 1e-3 # off track = bad
+    elif speed < SPEED_THRESHOLD:
+        # Penalize if the car goes too slow
+        reward *= 0.6
+    #else:
+        # High reward if the car stays on track and goes fast
+        # leave reward high
 
     return float(reward)
+
+
