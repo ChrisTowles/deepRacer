917ff928a988c9df24997e2f75cffeaafc695c44
Chris Towles <Chris.Towles@ge.com>
2021-08-17 16:29:08 -0400

    init function

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# deepRacer
+# Deep Racer
+
+Created repo as part of GE Session on 08-17-2021
+
+Docs on reward function
+<https://docs.aws.amazon.com/deepracer/latest/developerguide/deepracer-reward-function-input.html>
+
+AWS DeepRacer Slack community
+<http://join.deepracing.io/>
+
+GitHub
+<https://github.com/aws-samples/aws-deepracer-workshops/>
+
+Free video course
+<https://www.aws.training/Details/eLearning?id=32143>
+
+Tips
+<https://aws.amazon.com/deepracer/racing-tips/>
+
+Reward function parameters
+<https://docs.aws.amazon.com/deepracer/latest/developerquide/deepracer-reward-function-input.html>
---
+++ reward_function.py
@@ -0,0 +1,68 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+
+    "all_wheels_on_track": Boolean,        # flag to indicate if the agent is on the track
+    "x": float,                            # agent's x-coordinate in meters
+    "y": float,                            # agent's y-coordinate in meters
+    "closest_objects": [int, int],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
+    "closest_waypoints": [int, int],       # indices of the two nearest waypoints.
+    "distance_from_center": float,         # distance in meters from the track center 
+    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not. 
+    "is_offtrack": Boolean,                # Boolean flag to indicate whether the agent has gone off track.
+    "is_reversed": Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    "heading": float,                      # agent's yaw in degrees
+    "objects_distance": [float, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
+    "objects_heading": [float, ],          # list of the objects' headings in degrees between -180 and 180.
+    "objects_left_of_center": [Boolean, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
+    "objects_location": [(float, float),], # list of object locations [(x,y), ...].
+    "objects_speed": [float, ],            # list of the objects' speeds in meters per second.
+    "progress": float,                     # percentage of track completed
+    "speed": float,                        # agent's speed in meters per second (m/s)
+    "steering_angle": float,               # agent's steering angle in degrees
+    "steps": int,                          # number steps completed
+    "track_length": float,                 # track length in meters.
+    "track_width": float,                  # width of the track
+    "waypoints": [(float, float), ]        # list of (x,y) as milestones along the track center
+    '''
+
+    # Read input parameters
+    all_wheels_on_track: bool = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    
+    speed = params['speed']
+    steering = abs(params['steering_angle'])
+    flag_unsafe = params['flag_unsafe']
+    
+    
+    reward = 1e-3  # likely crashed / close to off track
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+
+    
+
+
+    return float(reward)
